27ee750b3633bf39ae34e8259e2115b6023cbccd
Prahalad Mankoliya <prahaladj12345@gmail.com>
2023-12-15 10:16:23 +0530

    changes

--- React/my-react-app/src/components/Expensis.jsx
+++ React/my-react-app/src/components/Expensis.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const Expensis = () => {
-  return (
-    <ul>
-        <li>data 1</li>
-        <li>data 2</li>
-        <li>data 3</li>
-    </ul>
-  )
+    return (
+        <ul>
+            <li>data 1</li>
+            <li>data 2</li>
+            <li>data 3</li>
+        </ul>
+    )
 }
 
 export default Expensis
