0eddc109d6d0a842feaabb1397311c5459720b58
Prahalad Mankoliya <prahaladj12345@gmail.com>
2023-12-15 10:06:24 +0530

    add react

---
+++ React/my-react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Expensis from './components/Expensis'
+
+const App = () => {
+  return (
+    <>
+    <h1>The Main App</h1>
+    
+    <Expensis/>
+    </>
+  )
+}
+
+export default App
